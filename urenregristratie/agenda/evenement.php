bdcd6bc5236a585ed16b683682a279f7e90cf3bc
leonReon <238663@edu.rocfriespoort.nl>
2022-04-06 14:44:29 +0200

    hele evenementen page gemaakt

--- urenregristratie/agenda/evenement.php
+++ urenregristratie/agenda/evenement.php
@@ -9,24 +9,37 @@ include "../database.php";
 
 <head>
 
-<h1>event invul velden</h1>
+
+<link rel="stylesheet" href="evenementen.css">
+
+
+    <img src="IMG/Logo_HareWare.png" class="Logo">
+</div>    
+
+
 
 </head>
 
 <body>
     
-
+<div>
+<img src="IMG/Background-Mac.jpg" class="background">
+  </div>
     
     <div class="inputvelden">
+   <h1>Event here</h1>
     <form method="post" action="../agenda/actionpage.php">
-      <label for="fname">First event:</label>
-      <input type="text" id="fname" name="fname"><br><br>
-      <label for="fname">date:</label>
-      <input type="date" id="fname" name="date"><br><br>
-      <label for="fname">hour:</label>
-      <input type="number" id="fname" name="hour" max="24" min="0"><br><br>
       
-      <input type="submit" value="Submit">
+    <label for="fname">Event:</label>
+      <input class="firstevent" type="text" id="fname" name="fname"><br><br>
+      
+      <label for="fname">Date:</label>
+      <input class="date" type="date" id="fname" name="date"><br><br>
+      
+      <label for="fname">Hour:</label>
+      <input class="hour" type="number" id="fname" name="hour" max="24" min="0"><br><br>
+      
+      <input class="button" type="submit" value="Submit">
 
     </form>
 
