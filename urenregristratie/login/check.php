565c9e4919fdbd65ea0c980d22a28146b7ee2012
sandje05 <sanderbroekhof05@outlook.com>
2022-04-08 10:02:25 +0200

    s

--- urenregristratie/login/check.php
+++ urenregristratie/login/check.php
@@ -8,16 +8,13 @@ $stmt = $connection->prepare("SELECT Password, Username FROM medewerker WHERE Us
 $stmt->execute([$_POST['user']]);
 $user = $stmt->fetch();
 
-var_dump($_POST['wachtwoord']);
-var_dump($user['Password']);
-var_dump(password_verify('admin', $user['Password']));
-die();
 
 if ($user && password_verify($_POST['wachtwoord'], $user['Password']))
 {
-    echo "Goed!";
+    header('Location: ../agenda/agenda.html');
 } else {
-    echo "Fout!";
+   
+    header('Location: ../login/index.php/');
 }
 
 
