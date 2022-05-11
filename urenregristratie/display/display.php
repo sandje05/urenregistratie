1742ec1ec235499c89fcec33ac142feb6af76543
sandje05 <sanderbroekhof05@outlook.com>
2022-05-11 14:46:20 +0200

    s

---
+++ urenregristratie/display/display.php
@@ -0,0 +1,27 @@
+<?php
+include '../database.php';
+
+
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="../display/displaydata.php" method="post" >
+        <input type="date" name="date1" id="date1">
+        <button type="submit"></button>
+    </form>
+</body>
+</html>
