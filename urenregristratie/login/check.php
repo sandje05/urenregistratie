5769e5de8c47da958c4db13f331e3d1e0bb2f911
sandje05 <sanderbroekhof05@outlook.com>
2022-05-13 11:09:46 +0200

    s

--- urenregristratie/login/check.php
+++ urenregristratie/login/check.php
@@ -3,8 +3,8 @@ include '../database.php'; // hier include ik voor database connectie
 session_start();
 
 
-
-$stmt = $connection->prepare("SELECT Password, Username, ID_worker  FROM medewerker WHERE Username = ?");
+$level = "1";
+$stmt = $connection->prepare("SELECT Password, Username, ID_worker, Level  FROM medewerker WHERE Username = ?");
 $stmt->execute([$_POST['user']]); //execute om data uit database te halen
 $user = $stmt->fetch();
 
@@ -12,14 +12,21 @@ $_SESSION["id"] = $user['ID_worker'];
 $_SESSION["name"] = $user['Username'];
 if ($user && password_verify($_POST['wachtwoord'], $user['Password'])) //password check doormiddel van password verify 
 {
+    if($user["Level"] == $level){
+        header('Location: ../login/nope/nope.php');
 
-if(isset($_SESSION["id"])){
-
-    header('Location: ../test/test.php');}
-} else {
-   
-    header('Location: ../login/index.php');
+    }else{
+        
+        if(isset($_SESSION["id"])){
+        header('Location: ../test/test.php');}
+    else{ 
+       
+        header('Location: ../login/index.php');}    
+        
+    }
 }
+
+
 
 
 
