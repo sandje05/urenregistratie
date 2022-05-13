3cb9375edcb3fa0e5245c48d89839e153bd330d4
sandje05 <sanderbroekhof05@outlook.com>
2022-05-13 09:49:31 +0200

    s

---
+++ urenregristratie/password/password.php
@@ -0,0 +1,31 @@
+<?php
+include '../database.php';
+
+
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="../password/change.php" method="post">
+<select name="change1" id="change2">
+<option value="volvo">Volvo</option>
+</select>
+
+    </form>
+</body>
+</html>
